1fe8ffe9b26c1fb747a404070898a5cc5537ea06
xlxcocoxlx <pollocesar2007@gmail.com>
2024-02-08 08:35:26 -0600

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,87 @@
+
+#TODO: hangan
+#TODO: Turtle for hangman, draw the hangman
+#TODO: List for words in categories
+#TODO: prompts for hte words
+#TODO:  check if hte letter submitted is in the word
+#TODO: option to make a guess of the word
+#TODO: Menu to choose the category
+#TODO: draw the body parts for the hangman on hte turtle
+from turtle import *
+from time import sleep
+
+testWord = "test"
+letterSubmitted = None
+limbs = 0
+
+
+def newGame():
+    width(5)
+    Screen()
+    penup()
+    backward(100)
+    pendown()
+    backward(50)
+    forward(25)
+    left(90)
+    forward(200)
+    right(90)
+    forward(100)
+    right(90)
+    forward(20)
+    game()
+
+def drawLimbs():
+    global limbs
+    if limbs == 0:
+        #Head
+        right(90)
+        circle(17)
+    elif limbs == 1:
+        #Body
+        left(90)
+        penup()
+        forward(34)
+        pendown()
+        forward(100)
+    elif limbs == 2:
+        #right leg
+        left(45)
+        forward(25)
+        backward(25)
+    elif limbs == 3:
+        #Left leg
+        right(90)
+        forward(25)
+        backward(25)
+        right(135)
+    elif limbs == 4:
+        #left arm
+        forward(80)
+        left(90)
+        forward(25)
+    elif limbs == 5:
+        #right arm
+        backward(80)
+        gameOver()
+    limbs = limbs + 1
+    print(limbs)    
+    game()
+     
+def gameOver():
+    #make turtle draw gameover
+    print("Gameover")
+    sleep(2)
+    quit()
+
+
+def game():
+    print("guess the letter of the word")
+    letterSubmitted = input()
+    if letterSubmitted in testWord:
+        game()
+    else:
+        drawLimbs()  
+    
+
+newGame()
