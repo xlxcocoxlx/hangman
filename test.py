697ae5f52a31b970a99897c95514f6ae00ef2b32
xlxcocoxlx <pollocesar2007@gmail.com>
2024-02-08 08:42:09 -0600

    n

---
+++ test.py
@@ -0,0 +1,26 @@
+from turtle import Screen, Turtle
+
+CURSOR_SIZE = 20
+FONT_SIZE = 12
+FONT = ('Arial', FONT_SIZE, 'bold')
+
+def draw_onclick(x, y):
+    turtle.dot(100, 'cyan')
+
+button = Turtle()
+button.hideturtle()
+button.shape('square')
+button.shapesize(2,2,1)
+button.fillcolor('red')
+button.penup()
+button.goto(150, 150)
+button.write("Click me!", align='center', font=FONT)
+button.sety(150 + CURSOR_SIZE + FONT_SIZE)
+button.onclick(draw_onclick)
+button.showturtle()
+
+turtle = Turtle()
+turtle.hideturtle()
+
+screen = Screen()
+screen.mainloop()
